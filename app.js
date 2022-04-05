a439fcb714a2f7bd4ada64e81a3516f742c7a2de
sergkryk <sergkryk@ya.ru>
2022-04-05 19:11:01 +0300

    2nd commit

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 var express = require('express');
 var http = require('http');
-var os = require('os');
-
-console.log(os.networkInterfaces());
 
 var app = express();
 var server = http.createServer(app);
 
-app.get('/', function(req, res) {
-    console.log('hello');	
+app.get('/', function(req, res) {	
     res.send("Hello World!");
 });
 
-server.listen(3000, '192.168.88.248');
+server.listen(3000);
 server.on('listening', function() {
     console.log('Express server started on port %s at %s', server.address().port, server.address().address);
 });
