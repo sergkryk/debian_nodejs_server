ae2bc27078fb2470c6e518a49831ae6307a46808
sergkryk <sergkryk@ya.ru>
2022-05-26 20:46:39 +0300

    removes additional checks

--- middleware/tokenVerification.js
+++ middleware/tokenVerification.js
@@ -2,22 +2,17 @@ const jwt = require("jsonwebtoken");
 const { secret } = require("../config/secret");
 
 const authenticateToken = function(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const bearer = authHeader.split(' ');
-  const token = bearer[1];
-  
-  if(!token) {
-    res.sendStatus(401)
-  }
-
   try {
-    const decoded = jwt.verify(token, secret, {algorithms: ['HS256']});
-    console.log(decoded);
+    const authHeader = req.headers['authorization']
+    const bearer = authHeader.split(' ');
+    const token = bearer[1];
+    const decoded = jwt.verify(token, secret, { algorithms: ['HS256'] });
     req.params.uid = decoded.id;
     next();
   } 
   catch (error) {
-    res.status(403).send({"message": error.message});
+    console.log(error);
+    res.status(401).send();
   }
 }
 
