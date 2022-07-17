413bb59fa7c6ff684606337aea9c8245c8af0cde
sergkryk <sergkryk@ya.ru>
2022-07-17 17:04:12 +0300

    adds func

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,5 +1,4 @@
 const CardModel = require('../models/cards');
-// const Actions = require('../models/actions');
 
 module.exports = class {
   static async getById(req, res) {
@@ -21,13 +20,6 @@ module.exports = class {
       const payCard = await CardModel.check({serial, pin});
       await CardModel.use(uid, payCard, ip);
       res.status(200).json(payCard)
-      // const ip = req.ip;
-      // await checkPrevious(id, previous);
-      // checkCandidate(candidate);
-      // checkConfirmed(candidate, confirmed);
-      // await checkPasswordDate(id);
-      // await Model.update(id, ip, candidate);
-      // res.status(200).json({message: 'Данные успешно сохранены'});
     } catch (error) {
       res.status(200).json({
         message: error.message,
