86bdf02955bd5f9280dbf3516c719df1e3c5a7fd
sergkryk <sergkryk@ya.ru>
2022-08-07 08:54:48 +0300

    debugging

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ const app = express();
 //   origin: `http://localhost:8080`,
 //   credentials: true,
 // }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
