893dadfcbd154d000c1ca6cd703c7e789c04403d
sergkryk <sergkryk@ya.ru>
2022-08-07 09:12:57 +0300

    debugging

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ const app = express();
 //   credentials: true,
 // }));
 app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
