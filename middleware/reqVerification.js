bceac73a07c27c09d6df69ad5e22c6a38c975f82
sergkryk <sergkryk@ya.ru>
2023-02-19 17:45:20 +0300

    adds regexp validation

--- middleware/reqVerification.js
+++ middleware/reqVerification.js
@@ -1,20 +1,45 @@
 const types = ["check", "pay", "cancel"];
 
-const XmlResponse = require('../utils/xml.js');
+const xmlResponse = require("../utils/xml.js");
+
+const accRegExp = new RegExp(/^\d{4}$/);
+const trRegExp = new RegExp(/^\d{1,}$/);
+const dateRegExp = new RegExp(/^\d{14}$/);
+const amountRegExp = new RegExp(/^\d{1,5}\.\d{2}$/);
 
 const reqVerification = function (req, res, next) {
   try {
     const { QueryType, TransactionId, Account } = req.query;
-    if (!QueryType || !TransactionId || !Account) {
-      throw new Error(299);
+    const isType = types.includes(QueryType);
+    const isAccount = accRegExp.test(Account);
+    const isTransaction = trRegExp.test(TransactionId);
+
+    if ([isType, isAccount, isTransaction].includes(false)) {
+      throw new Error();
     }
-    if (!types.includes(QueryType)) {
-      throw new Error(299);
+    if (QueryType === "pay") {
+      const { TransactionDate, Amount } = req.query;
+
+      const isDate = dateRegExp.test(TransactionDate);
+      const isAmount = amountRegExp.test(Amount);
+
+      if (!isDate || !isAmount) {
+        throw new Error();
+      }
+    }
+    if (QueryType === "cancel") {
+      const { Amount } = req.query;
+
+      const isAmount = amountRegExp.test(Amount);
+
+      if (!isAmount) {
+        throw new Error();
+      }
     }
     next();
   } catch (error) {
-    res.header("Content-Type", "application/xml");
-    res.status(200).send(XmlResponse.cancel());
+    res.set("Content-Type", "text/xml");
+    res.send(xmlResponse(3, "Check your query format!"));
   }
 };
 
