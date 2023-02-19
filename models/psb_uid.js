6c46905688c592f851ced5a3f277c00d3ac8df7d
sergkryk <sergkryk@ya.ru>
2023-02-19 19:16:45 +0300

    refactoring

--- models/psb_uid.js
+++ models/psb_uid.js
@@ -1,13 +1,13 @@
 const pool = require("../utils/db");
 
 async function fetchUid(id) {
-  try {
-    const [[res]] = await pool.execute(
-      `SELECT uid FROM users WHERE id = 'user_${id}'`
-    );
-    return res;
-  } catch (error) {
-    throw new Error(error);
+  const [[ data ]] = await pool.execute(
+    `SELECT uid FROM users WHERE id = 'user_${id}'`
+  );
+  if (data) {
+    return data;
+  } else {
+    return "";
   }
 }
 
