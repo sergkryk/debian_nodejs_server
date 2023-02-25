816ad494c114180e169e04c890e27ea40b5aea67
sergkryk <sergkryk@ya.ru>
2023-02-25 22:48:27 +0300

    refactoring

--- controllers/sms.js
+++ controllers/sms.js
@@ -4,6 +4,8 @@ const TarifModel = require("../models/tarifs");
 const BillsModel = require("../models/bills");
 const PiModel = require("../models/pi");
 
+const chunkSize = 25;
+
 // вспомогательная функция для проверки телефона
 function validatePhone(el) {
   const exp = /72\d{7}/;
@@ -14,21 +16,30 @@ function validatePhone(el) {
 // вспомогательная функция генерации даты с первым числом следующего месяца
 function getDate() {
   // создаю обьект текущей даты
-  const now = new Date;
+  const now = new Date();
   // устанавливаю следующий месяц и первое число и получаю миллисекунды
   const milliseconds = now.setMonth(now.getMonth() + 1, 1);
   // возвращаю сконвертированные миллисекунды с российской формате
-  return new Date(milliseconds).toLocaleDateString('ru-RU');
+  return new Date(milliseconds).toLocaleDateString("ru-RU");
 }
 
-// функция редьюсер для редьюса массива содержащего айди тарифных планов
-async function reminder(acc, item) {
-  // нужно всегда ресолвить аккумулятор в редьюсе т.к. редьюс асинхронный
-  const resolvedAcc = await acc;
+// вспомогательная функция деления массива на чанки
+function chunkArray(myArray, chunk_size) {
+  const tempArray = [];
+
+  for (let i = 0; i < myArray.length; i += chunk_size) {
+    myChunk = myArray.slice(i, i + chunk_size);
+    tempArray.push(myChunk);
+  }
+  return tempArray;
+}
+
+async function reminder(tp_ip) {
+  const numbers = [];
   // получаю обьект с текущим тарифом т.к. мне нужна сумма платежа для сравнения с депозитом пользователя
-  const tarif = await TarifModel.fetchById(item.id);
+  const tarif = await TarifModel.fetchById(tp_ip);
   // получаю массив с вложенными массивами уидов абонентов по каждому ежемесячному тарифу
-  const uidLists = await TarifModel.fetchUsersByTarif(item.id);
+  const uidLists = await TarifModel.fetchUsersByTarif(tp_ip);
   // запускаю цикл внутри цикла (знаю это плохо) в котором обрабатываю полученные уиды абонентов
   for (let i = 0; i < uidLists.length; i++) {
     // получаю счёт пользователя с его депозитом
@@ -41,34 +52,39 @@ async function reminder(acc, item) {
       const validated = validatePhone(phone[0].phone);
       // если номер валиден значит могу отправить абоненту смс с предупреждением, добавляю номер в список для дальнейшей отправки
       if (validated && validated[0]) {
-        const number = validated[0];
-        resolvedAcc.push(`7959${number.slice(number.length - 7)}`);
+        const number = `7959${validated[0].slice(validated[0].length - 7)}`;
+        numbers.push(number);
       }
     }
   }
-  return resolvedAcc;
+  return numbers;
 }
 
 // функция для ежемесячной отправки смс
 async function monthlyReminder() {
+  const message = `Обратите внимание! ${getDate()} списание абонентской платы за интернет.`;
+
   // получаю список содержащий айди тарифных планов с ежемесячной оплатой
   const tarifsIds = await TarifModel.fetchAllMonthly();
-  /* с помощью встроенной функции редьюс трансформирую список содержащий айди тарифных планов в список абонентов,
-     провожу их проверку и отправляю смс при необходимости. см. getUsersLists
-  */
-  const numbers = await tarifsIds.reduce(reminder, Promise.resolve([]));
-  const message = `Обратите внимание! ${getDate()} списание абонентской платы за интернет.`
-  // использую библиотеку от sms.ru отправляю смс всем абонентам с предупреждением
-  sms.sms_send(
-    {
-      to: numbers.slice(0,10).toString(),
-      text: message,
-      test: true,
-    },
-    function (e) {
-      console.log(e);
+
+  for (let i = 0; i < tarifsIds.length; i++) {
+    const numbers = await reminder(tarifsIds[i].id);
+    const chunkedNumbers = chunkArray(numbers, chunkSize);
+
+    // использую библиотеку от sms.ru отправляю смс всем абонентам с предупреждением
+    for (let i = 0; i < chunkedNumbers.length; i++) {
+    //   sms.sms_send(
+    //     {
+    //       to: chunkedNumbers[i].toString(),
+    //       text: message,
+    //       test: true,
+    //     },
+    //     function (e) {
+    //       console.log(e);
+    //     }
+    //   );
     }
-  );
+  }
 }
 
 module.exports = { monthlyReminder };
