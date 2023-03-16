30ea17c5e2a9f3cd9271a4bfcadba0a1ed93de4c
sergkryk <sergkryk@ya.ru>
2023-03-16 13:26:24 +0300

    mysql2 version update

--- utils/database.js
+++ utils/database.js
@@ -5,6 +5,8 @@ const config = {
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
+  keepAliveInitialDelay: 10000, // 0 by default.
+  enableKeepAlive: true, // false by default.
 };
 
 const pool = mysql.createPool(config);
@@ -17,5 +19,20 @@ async function dbQuery(queryString) {
   }
   return rows;
 }
+
+
+// функция для проверки ошибки ECONNRESET, вдруг будет нужна
+// async function query_with_retries(sql, params, retries_left=1) {
+//   try {
+//     return pool.query(sql, params)
+//   } catch(err) {
+//     if(retries_left >= 1 && err.code === 'ECONNRESET') {
+//       console.error({msg:'query_with_retries retrying!', retries_left, err})
+//       return query_with_retries(sql, params, retries_left-1)
+//     } else {
+//       throw err
+//     }
+//   }
+// }
 
 module.exports = dbQuery;
